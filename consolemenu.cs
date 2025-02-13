e0b10e679502a252161b80312d3300e7978edcb5
senabilgin <49808970+senabilgin@users.noreply.github.com>
2025-02-12 22:17:56 -0500

    first commit
    created cs file and primted menu options

---
+++ consolemenu.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("*******************");
+            Console.WriteLine("\nMenu: \n");
+            Console.WriteLine("1- New Game");
+            Console.WriteLine("2- Load Game");
+            Console.WriteLine("3- options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("\n**************");
+            Console.Write("Enter your choice: ");
+
+            string choice = Console.ReadLine();     
+            
+
+            switch (choice)
+               }
+    }
+}
